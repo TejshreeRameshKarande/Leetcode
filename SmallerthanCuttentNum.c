4bbe9fb703e8ec9742e23dd130c1749d83cc6763
TejshreeRameshKarande <tejshreekarande23@gmail.com>
2025-07-16 21:51:17 +0530

    Add files via upload

---
+++ SmallerthanCuttentNum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int a[n];
+	
+	printf("size:");
+	scanf("%d",&n);
+	
+	int i;
+	printf("List of Numbers:");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+}
